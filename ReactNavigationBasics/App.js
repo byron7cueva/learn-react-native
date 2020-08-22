74f68634c12c457d4568c4284891b80afb68d3bb
byron7cueva <byron7cueva@gmail.com>
2020-08-22 16:47:11 -0500

    Config more settings on stack navigation

--- ReactNavigationBasics/App.js
+++ ReactNavigationBasics/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import {
+  Text
+} from 'react-native'
 
 import { Home } from './src/screens/home'
 import { Login } from './src/screens/login'
@@ -9,30 +12,47 @@ import { Profile } from './src/screens/profile'
 
 const Stack = createStackNavigator()
 
- const App: () => React$Node = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName='Login'
-        screenOptions={{
-          title: 'Titulo generico'
+const App: () => React$Node = () => {
+return (
+  <NavigationContainer>
+    <Stack.Navigator
+      initialRouteName='Login'
+      screenOptions={{
+        title: 'Titulo generico', // Un titulo que se pone a todas las pantalla si estas no tienen configuradas la propiedad title
+        headerTitleAllowFontScaling: true, // Para que pueda escalar la fuente dependiendo de la configuracion del usuario
+        // Definir mi propio header
+        /* header: ({ scene, previous, navigation }) => {
+          return <Text>Esto es un header</Text>
+        }*/
+        headerBackTitle: 'Atras', // Texto que le acompaña al icono back
+        gestureEnabled: true, // Habilitar los gestos de arrastrar para regresar
+        // Redefiniendo el boton back
+        headerBackImage: () => {
+          return <Text>Atr</Text>
+        },
+        cardStyle: {
+          borderWidth: 2,
+          backgroundColor: 'red'
+        }
+      }}
+      headerMode='screen'
+      mode='card'
+    >
+      <Stack.Screen name='Home' component={Home} />
+      <Stack.Screen name='About' component={About} />
+      <Stack.Screen 
+        name='Login'
+        options={{
+          title: 'Pantalla Login'
         }}
-      >
-        <Stack.Screen name='Home' component={Home} />
-        <Stack.Screen name='About' component={About} />
-        <Stack.Screen 
-          name='Login'
-          options={{
-            title: 'Pantalla Login'
-          }}
-          component={Login}
-          key='login'
-          initialParams={{ name: 'Byron' }}
-        />
-        <Stack.Screen name='Profile' component={Profile} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+        component={Login}
+        key='login'
+        initialParams={{ name: 'Byron' }}
+      />
+      <Stack.Screen name='Profile' component={Profile} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 };
 
 export default App;
