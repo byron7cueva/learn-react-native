1eb782e29db8ea052d161f2fb016d9a311b15ddd
byron7cueva <byron7cueva@gmail.com>
2020-08-22 20:23:22 -0500

    Combined routes

--- ReactNavigationBasics/App.js
+++ ReactNavigationBasics/App.js
@@ -10,55 +10,67 @@ import { Login } from './src/screens/login'
 import { About } from './src/screens/about'
 import { Profile } from './src/screens/profile'
 
-const Stack = createStackNavigator()
+const HomeStack = createStackNavigator()
 
+const HomeStackScreen = () => (
+  <HomeStack.Navigator
+    initialRouteName='Home'
+    screenOptions={{
+      title: 'Titulo generico', // Un titulo que se pone a todas las pantalla si estas no tienen configuradas la propiedad title
+      headerTitleAllowFontScaling: true, // Para que pueda escalar la fuente dependiendo de la configuracion del usuario
+      // Definir mi propio header
+      /* header: ({ scene, previous, navigation }) => {
+        return <Text>Esto es un header</Text>
+      }*/
+      headerBackTitle: 'Atras', // Texto que le acompaña al icono back
+      gestureEnabled: true, // Habilitar los gestos de arrastrar para regresar
+      // Redefiniendo el boton back
+      headerBackImage: () => {
+        return <Text>Atr</Text>
+      },
+      cardStyle: {
+        borderWidth: 2,
+        backgroundColor: 'red'
+      }
+    }}
+    headerMode='screen'
+    mode='card'
+  >
+    <HomeStack.Screen name='Home' component={Home} />
+    <HomeStack.Screen name='About' component={About} />
+    <HomeStack.Screen
+      name='Profile'
+      component={Profile}
+      options={
+        ({ route }) => ({
+          title: `${route.params.name} ${route.params?.age ?? '32'}`
+        })
+      }
+    />
+  </HomeStack.Navigator>
+)
+
+const AppStack = createStackNavigator()
+
+// Combinando rutas
 const App: () => React$Node = () => {
 return (
   <NavigationContainer>
-    <Stack.Navigator
-      initialRouteName='Login'
-      screenOptions={{
-        title: 'Titulo generico', // Un titulo que se pone a todas las pantalla si estas no tienen configuradas la propiedad title
-        headerTitleAllowFontScaling: true, // Para que pueda escalar la fuente dependiendo de la configuracion del usuario
-        // Definir mi propio header
-        /* header: ({ scene, previous, navigation }) => {
-          return <Text>Esto es un header</Text>
-        }*/
-        headerBackTitle: 'Atras', // Texto que le acompaña al icono back
-        gestureEnabled: true, // Habilitar los gestos de arrastrar para regresar
-        // Redefiniendo el boton back
-        headerBackImage: () => {
-          return <Text>Atr</Text>
-        },
-        cardStyle: {
-          borderWidth: 2,
-          backgroundColor: 'red'
-        }
-      }}
-      headerMode='screen'
-      mode='card'
+    <AppStack.Navigator
+      mode='modal'
+      headerMode='none'
     >
-      <Stack.Screen name='Home' component={Home} />
-      <Stack.Screen name='About' component={About} />
-      <Stack.Screen 
-        name='Login'
-        options={{
-          title: 'Pantalla Login'
-        }}
-        component={Login}
-        key='login'
-        initialParams={{ name: 'Byron' }}
-      />
-      <Stack.Screen
-        name='Profile'
-        component={Profile}
-        options={
-          ({ route }) => ({
-            title: `${route.params.name} ${route.params?.age ?? '32'}`
-          })
-        }
-      />
-    </Stack.Navigator>
+      <AppStack.Screen name='Home' component={HomeStackScreen} />
+      <AppStack.Screen 
+      name='Login'
+      options={{
+        title: 'Pantalla Login'
+      }}
+      component={Login}
+      key='login'
+      initialParams={{ name: 'Byron' }}
+    />
+    </AppStack.Navigator>
   </NavigationContainer>
 );
 };
