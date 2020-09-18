ff0bd1f62213a3ce248a7b1ae89f206108cc4ccf
byron7cueva <byron7cueva@gmail.com>
2020-09-18 14:28:28 -0500

    cryptoTracker - Favorite section

--- cryctoTracker/App.js
+++ cryctoTracker/App.js
@@ -5,36 +5,47 @@ import {Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import CoinsStack from './src/components/coins/CoinsStack';
 import Colors from './src/res/colors';
+import CoinsStack from './src/components/coins/CoinsStack';
+import FavoritesStack from './src/components/favorites/FavoritesStack';
 
 const Tabs = createBottomTabNavigator();
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Tabs.Navigator
-        tabBarOptions={{
-          tintColor: '#fefefe',
-          style: {
-            backgroundColor: Colors.blackPearl,
-          },
-        }}>
-        <Tabs.Screen
-          name="Coins"
-          component={CoinsStack}
-          options={{
-            tabBarIcon: ({size, color}) => (
-              <Image
-                style={{tintColor: color, width: size, height: size}}
-                source={require('./src/assets/bank.png')}
-              />
-            ),
-          }}
-        />
-      </Tabs.Navigator>
-    </NavigationContainer>
-  );
-};
+const App = () => (
+  <NavigationContainer>
+    <Tabs.Navigator
+      tabBarOptions={{
+        tintColor: '#fefefe',
+        style: {
+          backgroundColor: Colors.blackPearl,
+        },
+      }}>
+      <Tabs.Screen
+        name="Coins"
+        component={CoinsStack}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Image
+              style={{tintColor: color, width: size, height: size}}
+              source={require('./src/assets/bank.png')}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Favorites"
+        component={FavoritesStack}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Image
+              style={{tintColor: color, width: size, height: size}}
+              source={require('./src/assets/star.png')}
+            />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  </NavigationContainer>
+);
 
 export default App;
