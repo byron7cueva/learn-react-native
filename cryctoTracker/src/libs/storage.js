7b799eb4a3d54b4d1327833cb21f369e4502def2
byron7cueva <byron7cueva@gmail.com>
2020-09-18 14:47:49 -0500

    cryptoTracker - Add storage lib

---
+++ cryctoTracker/src/libs/storage.js
@@ -0,0 +1,54 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+class Storage {
+  static instance = Storage();
+
+  async store(key, value) {
+    try {
+      await AsyncStorage.setItem(key, value);
+      return true;
+    } catch (err) {
+      console.log('storage store err', err);
+      return false;
+    }
+  }
+
+  async get(key) {
+    try {
+      return await AsyncStorage.getItem(key);
+    } catch (err) {
+      console.log('storage get err', err);
+      throw new Error(err);
+    }
+  }
+
+  async multiGet(keys) {
+    try {
+      return await AsyncStorage.multiGet(keys);
+    } catch (err) {
+      console.log('storage multiGet err', err);
+      throw new Error(err);
+    }
+  }
+
+  async getAllKeys() {
+    try {
+      return await AsyncStorage.getAllKeys();
+    } catch (err) {
+      console.log('storage getAllKeys err', err);
+      throw new Error(err);
+    }
+  }
+
+  async remove(key) {
+    try {
+      await AsyncStorage.removeItem(key);
+      return true;
+    } catch (err) {
+      console.log('storage remove err', err);
+      return false;
+    }
+  }
+}
+
+export default Storage;
