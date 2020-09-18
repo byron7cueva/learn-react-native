5c789464f7baea1f694e55735cb8f14062a45b3d
byron7cueva <byron7cueva@gmail.com>
2020-09-18 13:58:32 -0500

    cryptoTracker - Add tabs

--- cryctoTracker/App.js
+++ cryctoTracker/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import {Image} from 'react-native';
 // NavigationContainer: Nos ayuda a mantener el estado de la navegacion en general
 // tanto de tabs como de stack
 import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import CoinsStack from './src/components/coins/CoinsStack';
+import Colors from './src/res/colors';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: '#fefefe',
+          style: {
+            backgroundColor: Colors.blackPearl,
+          },
+        }}>
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={{
+            tabBarIcon: ({size, color}) => (
+              <Image
+                style={{tintColor: color, width: size, height: size}}
+                source={require('./src/assets/bank.png')}
+              />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
