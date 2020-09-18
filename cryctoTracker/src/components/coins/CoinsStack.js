932fcc127fde1377ffcb7e7345ab3fd067a0f101
byron7cueva <byron7cueva@gmail.com>
2020-09-18 14:27:47 -0500

    cryptoTracker - Change style white stateless component

--- cryctoTracker/src/components/coins/CoinsStack.js
+++ cryctoTracker/src/components/coins/CoinsStack.js
@@ -7,20 +7,18 @@ import Colors from '../../res/colors';
 
 const Stack = createStackNavigator();
 
-const CoinsStack = () => {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Colors.blackPearl,
-          shadowColor: Colors.blackPearl,
-        },
-        headerTintColor: Colors.white,
-      }}>
-      <Stack.Screen name="Coins" component={CoinsScreen} />
-      <Stack.Screen name="CoinDetail" component={CoinDetailScreen} />
-    </Stack.Navigator>
-  );
-};
+const CoinsStack = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: Colors.blackPearl,
+        shadowColor: Colors.blackPearl,
+      },
+      headerTintColor: Colors.white,
+    }}>
+    <Stack.Screen name="Coins" component={CoinsScreen} />
+    <Stack.Screen name="CoinDetail" component={CoinDetailScreen} />
+  </Stack.Navigator>
+);
 
 export default CoinsStack;
