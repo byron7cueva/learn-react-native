535beeb74f5ce86e34c1903d87701fc67f3b7b50
byron7cueva <byron7cueva@gmail.com>
2020-09-18 15:25:35 -0500

    cryptoTracker - Fixed bug on storage lib

--- cryctoTracker/src/libs/storage.js
+++ cryctoTracker/src/libs/storage.js
@@ -1,7 +1,7 @@
 import AsyncStorage from '@react-native-community/async-storage';
 
 class Storage {
-  static instance = Storage();
+  static instance = new Storage();
 
   async store(key, value) {
     try {
